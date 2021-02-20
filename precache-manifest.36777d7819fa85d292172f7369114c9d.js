d4c168d8c5af29fea35dc5b6b219786a3f0329f0
Usama Bin khalil <60216320+ubkch@users.noreply.github.com>
2021-02-20 17:43:05 +0500

    Updates

---
+++ precache-manifest.36777d7819fa85d292172f7369114c9d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "9403bda958cca405fe8f",
+    "url": "/TODO-ReactJS/static/css/main.a9c84f50.chunk.css"
+  },
+  {
+    "revision": "9403bda958cca405fe8f",
+    "url": "/TODO-ReactJS/static/js/main.9403bda9.chunk.js"
+  },
+  {
+    "revision": "430fff404ab81c508101",
+    "url": "/TODO-ReactJS/static/js/1.430fff40.chunk.js"
+  },
+  {
+    "revision": "9053d2b3bdbf319038c8",
+    "url": "/TODO-ReactJS/static/js/2.9053d2b3.chunk.js"
+  },
+  {
+    "revision": "57782ed66eb0dc84cb14",
+    "url": "/TODO-ReactJS/static/js/runtime~main.57782ed6.js"
+  },
+  {
+    "revision": "2151282078f57dd72913dea3b0613404",
+    "url": "/TODO-ReactJS/index.html"
+  }
+];
